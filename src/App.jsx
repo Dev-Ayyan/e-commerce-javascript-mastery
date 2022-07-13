a519c2bc1bbe1c992091e6a79d593fac65091b14
Dev-Ayyan <developerayyan@gmail.com>
2022-07-14 03:06:43 +0500

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Products from "./components/Products/Products";
+
+const App = () => {
+  return (
+    <div>
+      <h1>ECommerce</h1>
+    </div>
+  );
+};
+
+export default App;
